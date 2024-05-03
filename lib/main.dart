d4284aaf150e57092cbb79f370c0b3bfc4f364d3
ihsan <hasansaygin24@gmail.com>
2024-05-03 13:21:37 +0300

    models created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/models/product.dart
@@ -0,0 +1,49 @@
+class Product {
+  int id;
+  String title;
+  double price;
+  String description;
+  String category;
+  String image;
+  Rating rating;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json){
+    return Product(
+      id: json["id"], 
+      title: json["title"], 
+      price: json["price"] , 
+      description: json["description"] , 
+      category: json["category"] , 
+      image: json["image"] , 
+      rating: Rating.fromJson(json["rating"])  
+      );
+  }
+}
+
+
+class Rating{
+  double rate;
+  int count;
+
+  Rating({
+    required this.rate,
+    required this.count,
+  }
+    );
+
+   factory Rating.fromJson(Map<String, dynamic> json){
+     return Rating(
+       rate: json["rate"], 
+       count: json["count"]);
+   } 
+}
